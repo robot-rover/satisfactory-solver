dee480dcf4a007c478c1f35402ef30c596af2267
robot-rover <sam.obrien00@gmail.com>
2021-11-11 15:32:08 -0600

    Add recipe list window

--- solver/gui_main.py
+++ solver/gui_main.py
@@ -13,6 +13,7 @@ import yaml
 from . import game_parse, solve, visualize
 from .resources import ItemRate
 from .gui_recipes import AlternateRecipeWindow
+from .gui_recipe import RecipeListWindow
 
 OUTPUT_ICON = 'icons/Milestone/Recipe_Icon_Equipment_Dark.png'
 
@@ -76,17 +77,24 @@ class ItemSearchWidget(qtw.QLineEdit):
 
 
 class SchematicInputWidget(qtw.QWidget):
-    def __init__(self, item, group_widget, check_delete=True):
+    def __init__(self, item, group_widget, save_window_fn, game_data, check_delete=True):
         super().__init__()
         self.item = None
+        self.game_data = game_data
+        self.save_window_fn = save_window_fn
         self.group_widget = group_widget
         self.setFocusProxy(self.group_widget)
+
+        self.pixmap = None
+        self.icon = None
 
         layout = qtw.QHBoxLayout(self)
         layout.setContentsMargins(5, 5, 5, 5)
 
-        self.icon_label = qtw.QLabel()
+        self.icon_label = qtw.QPushButton()
+        self.icon_label.setFlat(True)
         self.icon_label.setFixedSize(50, 50)
+        self.icon_label.clicked.connect(self.open_recipe_window)
         layout.addWidget(self.icon_label, 0)
 
         self.group_box = qtw.QGroupBox()
@@ -103,9 +111,9 @@ class SchematicInputWidget(qtw.QWidget):
             self.setItem(item)
 
     @classmethod
-    def with_spinbox(cls, item, rate):
+    def with_spinbox(cls, item, rate, save_window_fn, game_data):
         group_widget = SchematicInputWidget.make_rate_box(rate)
-        return cls(item, group_widget)
+        return cls(item, group_widget, save_window_fn, game_data)
 
     @staticmethod
     def make_rate_box(rate):
@@ -114,6 +122,13 @@ class SchematicInputWidget(qtw.QWidget):
         group_widget.setMinimum(-2**31)
         group_widget.setValue(rate)
         return group_widget
+
+    def open_recipe_window(self):
+        if self.item is None:
+            return
+        window = RecipeListWindow(self.item, self.game_data)
+        window.show()
+        self.save_window_fn(window)
 
     def setItem(self, item):
         self.item = item
@@ -125,9 +140,11 @@ class SchematicInputWidget(qtw.QWidget):
             self.group_box.setTitle('')
 
     def setIcon(self, icon_path):
-        self.icon = qtg.QPixmap(icon_path).scaledToHeight(
+        self.pixmap = qtg.QPixmap(icon_path).scaledToHeight(
             50, qtc.Qt.SmoothTransformation)
-        self.icon_label.setPixmap(self.icon)
+        self.icon = qtg.QIcon(self.pixmap)
+        self.icon_label.setIcon(self.icon)
+        self.icon_label.setIconSize(self.pixmap.rect().size())
 
     def getItem(self):
         return self.item
@@ -203,6 +220,7 @@ class SatisfactorySolverMain(qtw.QApplication):
 
         self.solution = None
         self.current_file = None
+        self.windows = []
 
         self.w = qtw.QMainWindow()
         self.central_widget = qtw.QWidget()
@@ -240,7 +258,8 @@ class SatisfactorySolverMain(qtw.QApplication):
             'Set Target', self.item_lookup, qtg.QShortcut(qtg.QKeySequence(qtc.Qt.CTRL | qtc.Qt.Key_T), self.w))
         self.input_layout.addWidget(self.output_search)
         self.go_box = qtw.QPushButton("Go!")
-        self.output_show_box = SchematicInputWidget(None, self.go_box, False)
+        self.output_show_box = SchematicInputWidget(
+            None, self.go_box, self.save_window, self.game_data, False)
         self.output_show_box.setIcon(OUTPUT_ICON)
         self.input_layout.addWidget(self.output_show_box)
 
@@ -364,8 +383,15 @@ class SatisfactorySolverMain(qtw.QApplication):
                 item_widget.group_widget.setValue(
                     item_widget.group_widget.value() + rate)
                 return
-        widget = SchematicInputWidget.with_spinbox(item, rate)
+        widget = SchematicInputWidget.with_spinbox(
+            item, rate, self.save_window, self.game_data)
         self.input_list.insertWidget(self.input_list.count() - 1, widget)
+
+    def save_window(self, window):
+        for window in self.windows:
+            if window.isVisible():
+                print("Null")
+        self.windows.append(window)
 
     def get_problem(self):
         if self.output_show_box.getItem() is None:
